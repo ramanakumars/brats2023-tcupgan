4853773790802d3a27b37eaf08149a6dbafcff3a
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2023-08-11 19:14:56 -0500

    fixing flake styling

--- project/infer.py
+++ project/infer.py
@@ -1,77 +1,88 @@
 import torch
 from tcupgan.model import LSTMUNet
-from tcupgan.trainer import TrainerUNet
 from tcupgan.io import BraTSDataGenerator
 from torchvision import transforms
 from torchvision.transforms import Resize
 from tcupgan.utils import process_mask_tissue_wise
-import os, glob
+import os
+import glob
 import tqdm
 import nibabel as nib
 from einops import rearrange
 import numpy as np
-import scipy
-import cc3d
 
-def run_inference(data_dir: str, challenge_name:str, ckpt_file: str, output_dir: str) -> None:
-    assert (data_dir!='') or (ckpt_file!='') or (output_dir!=''), 'One or more input arguments are blank'
-    
+
+def run_inference(data_dir: str, challenge_name: str, ckpt_file: str, output_dir: str) -> None:
+    assert (data_dir != '') or (ckpt_file != '') or (
+        output_dir != ''), 'One or more input arguments are blank'
+
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
         print('Made missing output directory')
-        
+
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    
-    list_of_case_ids = ['-'.join(os.path.basename(folder).split('-')[2:4]) for folder in glob.glob('%s/*'%(data_dir))]
-    print(f'There are {len(list_of_case_ids)} case folders in the {data_dir} folder')
-    
+
+    list_of_case_ids = ['-'.join(os.path.basename(folder).split('-')[2:4])
+                        for folder in glob.glob('%s/*' % (data_dir))]
+    print(
+        f'There are {len(list_of_case_ids)} case folders in the {data_dir} folder')
+
     data = BraTSDataGenerator(data_dir)
     assert len(data) > 0, "No files detected!"
-    
+
     hidden = [16, 32, 48, 64, 128]
-    generator = LSTMUNet(hidden_dims=hidden, input_channels=4, output_channels=3).to(device)
+    generator = LSTMUNet(hidden_dims=hidden, input_channels=4,
+                         output_channels=3).to(device)
     generator.eval()
-    
+
     if device == 'cpu':
-        generator.load_state_dict(torch.load(f'{ckpt_file}',map_location=torch.device(device)))
+        generator.load_state_dict(torch.load(
+            f'{ckpt_file}', map_location=torch.device(device)))
         print('Loaded Model Weights successfully!')
     else:
         generator.load_state_dict(torch.load(f'{ckpt_file}'))
         print('Loaded Model Weights successfully!')
-        
-    resize_fn = Resize((240, 240), interpolation = transforms.InterpolationMode.NEAREST)
-    affine_matrix = np.array([[ -1.,  -0.,  -0.,   0.],[ -0.,  -1.,  -0., 239.],[  0.,   0.,   1.,   0.],[  0.,   0.,   0.,   1.]])
-    
+
+    resize_fn = Resize(
+        (240, 240), interpolation=transforms.InterpolationMode.NEAREST)
+    affine_matrix = np.array([[-1., -0., -0., 0.], [-0., -1., -0., 239.], [
+                             0., 0., 1., 0.], [0., 0., 0., 1.]])
+
     if challenge_name == 'PED':
         lesion_threshs = [75, 75, 25]
-        lesion_len_thresh = [5,5,5]
+        lesion_len_thresh = [5, 5, 5]
     elif challenge_name == 'GLI':
         lesion_threshs = [125, 75, 20]
-        lesion_len_thresh = [5,5,5]
+        lesion_len_thresh = [5, 5, 5]
     elif challenge_name == 'MEN':
         lesion_threshs = [125, 125, 25]
-        lesion_len_thresh = [5,5,5]
+        lesion_len_thresh = [5, 5, 5]
     elif challenge_name == 'SSA':
         lesion_threshs = [75, 100, 5]
-        lesion_len_thresh = [5,5,5]
-    
+        lesion_len_thresh = [5, 5, 5]
+
     counter = 1
     for each_sample in tqdm.tqdm(list_of_case_ids):
         ID, timepoint = each_sample.split('-')
         mri_cube, mask = data.get_from_ID(int(ID), int(timepoint))
         with torch.no_grad():
-            predicted_mask = torch.squeeze(generator(torch.unsqueeze(torch.Tensor(mri_cube).to(device), 0)),0).cpu().numpy()
-        
-        background = 1-torch.sum(torch.Tensor(predicted_mask), axis=1, keepdim=True)
-        combined_pred = np.concatenate([background.cpu().numpy(), predicted_mask], axis=1)
-        normed_combined_pred = torch.Tensor(combined_pred)/torch.sum(torch.Tensor(combined_pred), axis=(1), keepdim=True)
-        
-        combined_pred_mask = rearrange(resize_fn(torch.argmax(torch.Tensor(normed_combined_pred), dim=1)), 'z h w -> h w z' ).cpu().numpy()
-        
-        processed_mask = process_mask_tissue_wise(combined_pred_mask, mean_lesion_span_thresh=lesion_threshs, lesion_span_len_thresh=lesion_len_thresh)
-        
-        nii_file = nib.Nifti1Image(processed_mask, affine=affine_matrix, dtype='uint8')
-        nib.save(nii_file, f'{output_dir}/BraTS-{each_sample}.nii.gz')
-        counter+=1
-        
+            predicted_mask = torch.squeeze(generator(torch.unsqueeze(
+                torch.Tensor(mri_cube).to(device), 0)), 0).cpu().numpy()
 
+        background = 1 - \
+            torch.sum(torch.Tensor(predicted_mask), axis=1, keepdim=True)
+        combined_pred = np.concatenate(
+            [background.cpu().numpy(), predicted_mask], axis=1)
+        normed_combined_pred = torch.Tensor(
+            combined_pred) / torch.sum(torch.Tensor(combined_pred), axis=(1), keepdim=True)
+
+        combined_pred_mask = rearrange(resize_fn(torch.argmax(torch.Tensor(
+            normed_combined_pred), dim=1)), 'z h w -> h w z').cpu().numpy()
+
+        processed_mask = process_mask_tissue_wise(
+            combined_pred_mask, mean_lesion_span_thresh=lesion_threshs, lesion_span_len_thresh=lesion_len_thresh)
+
+        nii_file = nib.Nifti1Image(
+            processed_mask, affine=affine_matrix, dtype='uint8')
+        nib.save(nii_file, f'{output_dir}/BraTS-{each_sample}.nii.gz')
+        counter += 1
--- project/train.py
+++ project/train.py
@@ -20,15 +20,18 @@ def run_train(data_dir: str, config_file: str, ckpt_folder: str) -> None:
     batch_size = config.get('batch_size', 4)
     train_val_split = config.get('train_val_split', 0.9)
     start_from_last = config.get('start_from_last', False)
-    gen_transfer_learn_ckpt = config.get('generator_transfer_learn_checkpoint', '')
-    disc_transfer_learn_ckpt = config.get('discriminator_transfer_learn_checkpoint', '')
+    gen_transfer_learn_ckpt = config.get(
+        'generator_transfer_learn_checkpoint', '')
+    disc_transfer_learn_ckpt = config.get(
+        'discriminator_transfer_learn_checkpoint', '')
     gen_lr = config.get('generator_learning_rate', 1e-3)
     dsc_lr = config.get('discriminator_learning_rate', 1e-4)
     n_epochs = config.get('n_epochs', 50)
     norm = config.get('normalize', True)
     add_noise = config.get('add_noise', True)
 
-    assert not (start_from_last and gen_transfer_learn_ckpt != '' and gen_transfer_learn_ckpt !=''), 'cannot load from last save AND transfer learn from a different model'
+    assert not (start_from_last and gen_transfer_learn_ckpt != '' and gen_transfer_learn_ckpt !=
+                ''), 'cannot load from last save AND transfer learn from a different model'
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -37,39 +40,45 @@ def run_train(data_dir: str, config_file: str, ckpt_folder: str) -> None:
     assert len(data) > 0, "No files detected!"
 
     assert train_val_split < 1, "Training/Validation split must be less than 1!"
-    
-    torch_rand_gen = torch.Generator().manual_seed(9999)
-    train_datagen, val_datagen = random_split(data, [train_val_split, 1 - train_val_split], generator=torch_rand_gen)
 
-    train_data = DataLoader(train_datagen, num_workers=8, batch_size=batch_size, shuffle=True, pin_memory=True)
-    val_data = DataLoader(val_datagen,num_workers=8, batch_size=batch_size, shuffle=True, pin_memory=True)
+    torch_rand_gen = torch.Generator().manual_seed(9999)
+    train_datagen, val_datagen = random_split(
+        data, [train_val_split, 1 - train_val_split], generator=torch_rand_gen)
+
+    train_data = DataLoader(train_datagen, num_workers=8,
+                            batch_size=batch_size, shuffle=True, pin_memory=True)
+    val_data = DataLoader(val_datagen, num_workers=8,
+                          batch_size=batch_size, shuffle=True, pin_memory=True)
 
     hidden = [16, 32, 48, 64, 128]
-    generator = LSTMUNet(hidden_dims=hidden, input_channels=4, output_channels=3).to(device)
+    generator = LSTMUNet(hidden_dims=hidden, input_channels=4,
+                         output_channels=3).to(device)
 
-    discriminator = PatchDiscriminator(input_channels=7, nlayers=5, nfilt=16, activation='tanh', kernel_size=3).to(device)
+    discriminator = PatchDiscriminator(
+        input_channels=7, nlayers=5, nfilt=16, activation='tanh', kernel_size=3).to(device)
 
     summary(generator, input_size=(1, 155, 4, 256, 256), device=device)
     summary(discriminator, input_size=(1, 155, 7, 256, 256), device=device)
 
     checkpoint_path = os.path.join(ckpt_folder, run_name)
-    
+
     if not os.path.isdir(checkpoint_path):
         os.makedirs(checkpoint_path)
 
     trainer = TrainerUNet(generator, discriminator, checkpoint_path)
     if start_from_last:
         trainer.load_last_checkpoint()
-    if gen_transfer_learn_ckpt != '' and disc_transfer_learn_ckpt !='':
+    if gen_transfer_learn_ckpt != '' and disc_transfer_learn_ckpt != '':
         print('Transfer learning checkpoints provided')
 #         generator.load_transfer_data(gen_transfer_learn_ckpt)
 #         discriminator.load_transfer_data(disc_transfer_learn_ckpt)
         trainer.load(gen_transfer_learn_ckpt, disc_transfer_learn_ckpt)
-        print(f'Loaded {gen_transfer_learn_ckpt} \n {disc_transfer_learn_ckpt}')
+        print(
+            f'Loaded {gen_transfer_learn_ckpt} \n {disc_transfer_learn_ckpt}')
 
     gen_learning_rate = gen_lr
     dsc_learning_rate = dsc_lr
-    
+
     trainer.seg_alpha = 200
     trainer.loss_type = 'weigthed_bce'
 
